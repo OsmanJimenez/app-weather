82be70c4ef2c37276515781383e4b20fe1cf9489
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2021-09-12 19:11:23 -0500

    Feat: Add SearchBar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button'
 import { ThemeProvider } from '@material-ui/styles';
 import { Navbar } from './Components/Navbar'
 import { Feed } from './Components/Feed'
-import { Grid } from '@material-ui/core';
+import { Grid, TextField } from '@material-ui/core';
 import React, {useState, useEffect} from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -30,12 +30,16 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
+    width: '98%',
     marginTop: theme.spacing(3),
     overflowX: 'auto',
+    margin: 10
   },
 
   table: {
@@ -59,19 +63,29 @@ const useStyles = makeStyles(theme => ({
   avatar: {
     backgroundColor: red[500],
   },
+  TextField: {
+    width: 100,
+  },
 }));
 
 
 function App() {
   const baseUrl = "https://localhost:44336/api/Info";
   const baseUrl2 = "https://localhost:44336/api/Weather";
+
   const [data, setData]=useState([]);
+  const [datab, setDatab]=useState([]);
+  const [busqueda, setDataBus]=useState("");
+
   const [data2, setClima]=useState([]);
+  const [datab2, setClimab]=useState([]);
+  const [busqueda2, setClimaBus]=useState([]);
 
   const peticionNew= async() =>{
     await axios.get(baseUrl)
     .then(response=>{
       setData(response.data);
+      setDatab(response.data);
     }).catch(error=>{
       console.log("este es el error ", error);
     })
@@ -81,9 +95,39 @@ function App() {
     await axios.get(baseUrl2)
     .then(response=>{
       setClima(response.data);
+      setClimab(response.data);
     }).catch(error=>{
       console.log("este es el error ", error);
     })
+  }
+
+  const handleChange=e=>{
+    setDataBus(e.target.value);
+    setClimaBus(e.target.value);
+    filtrar(e.target.value);
+    filtrar2(e.target.value);
+  }
+
+  const filtrar=(terminoBusqueda)=>{
+    var resultadosBusqueda=datab.filter((elemento)=>{
+      if(elemento.city_name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      || elemento.title.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ){
+        return elemento;
+      }
+    });
+    setData(resultadosBusqueda);
+  }
+
+  const filtrar2=(terminoBusqueda)=>{
+    var resultadosBusqueda=datab2.filter((elemento)=>{
+      if(elemento.city_name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      || elemento.weather_Descriptions.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ){
+        return elemento;
+      }
+    });
+    setClima(resultadosBusqueda);
   }
 
   useEffect(()=>{
@@ -98,8 +142,16 @@ function App() {
     setExpanded(!expanded);
   };
 
+  
+
   return (
     <div>
+
+
+      <TextField id="standard-basic" label="Buscador" value={busqueda}
+          placeholder="Búsqueda por Ciudad o Titulo"
+          onChange={handleChange}/>
+
       <Paper className={classes.root}>
 
         <Table className={classes.table}>
@@ -121,7 +173,7 @@ function App() {
             {data2.map(row => (
               <TableRow key={row.id_Weather}>
                 <TableCell component="th" scope="row">
-                  {row.id_Weather}
+                  {row.city_name}
                 </TableCell>
                 <TableCell align="right">{row.observation_Time}</TableCell>
                 <TableCell align="right">{row.temperature }°</TableCell>
@@ -137,22 +189,17 @@ function App() {
         </Paper>
 
         <Grid container spacing={3}>
-          {data.map(row => (
+          {data && data.map(row => (
             <Grid item xs={3} key={row.Ciudad}>
             <Card className={classes.root}>
               <CardHeader
                 avatar={
                   <Avatar aria-label="recipe" className={classes.avatar}>
-                    O
+                    {row.author.charAt(0)}
                   </Avatar>
                 }
-                action={
-                  <IconButton aria-label="settings">
-                    <MoreVertIcon />
-                  </IconButton>
-                }
                 title={row.title}
-                subheader={row.published_At}
+                subheader={row.published_At.substr(0,10)}
               />
               <CardMedia
                 className={classes.media}
@@ -161,7 +208,7 @@ function App() {
               />
               <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  {row.description_New }
+                  {row.description_New.substr(0,63) }...
                 </Typography>
               </CardContent>
               <CardActions disableSpacing>
